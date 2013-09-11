4bca71559ce0c50cbcaa5b2bc17cb345ee82bcf0
Paul Tang <pauledangerously1@gmail.com>
2013-09-11 00:09:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an ARM EABI toolchain compiled on a Motorola Xoom using jsnyder's ARM EABI Toolchain Builder (https://github.com/jsnyder/arm-eabi-toolchain/). Confirmed working with Pebble SDK on a Motorola Xoom!
---
+++ lib/gcc/arm-none-eabi/4.7.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../gcc-4.7-2012.09/configure --prefix=/home/paul/arm-cs-tools --with-pkgversion='32-bit ARM EABI Toolchain JBS-2012.09-63-v2011.09-69-20-gfef160a' --with-bugurl=https://github.com/jsnyder/arm-eabi-toolchain --target=arm-none-eabi --enable-languages=c,c++ --with-gnu-ld --with-gnu-as --with-newlib --disable-nls --disable-libssp --disable-shared --enable-threads --with-headers=yes --disable-libmudflap --disable-libgomp --enable-lto --disable-libstdcxx-pch --enable-poison-system-directories --with-sysroot=/home/paul/arm-cs-tools/arm-none-eabi --with-build-time-tools=/home/paul/arm-cs-tools/arm-none-eabi/bin --enable-extra-sgxxlite-multilibs --with-specs='%{save-temps: -fverbose-asm} -D__CS_SOURCERYGXX_MAJ__=2012 -D__CS_SOURCERYGXX_MIN__=09 -D__CS_SOURCERYGXX_REV__=63 %{O2:%{!fno-remove-local-statics: -fremove-local-statics}} %{O*:%{O|O0|O1|O2|Os:;:%{!fno-remove-local-statics: -fremove-local-statics}}}'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
